714fbe558b918427e2d1c3ba3676fc4d680fef39
tiffany707 <tiffanyokura7@gmail.com>
2026-07-01 00:53:53 -0600

    Finished /api/categorization good path unit test

--- Backend/src/routes/transactions.test.js
+++ Backend/src/routes/transactions.test.js
@@ -1,8 +1,62 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi, beforeEach } from "vitest";
 import  request  from "supertest";
-import app from "../app.js"
 
-describe("POST api categorizer", async () => {
+
+
+//test OpenAI and the categorizer
+
+//Mock OpenAI
+vi.mock("../openaiService.js", () =>({
+    categorizeTransaction: vi.fn().mockResolvedValue("Subscriptions")
+}))
+
+
+
+vi.mock("../db.js", () => {
+    const mockClient = {
+            query: vi.fn(),
+            release: vi.fn()
+        }
+    return{
+        pool:{
+            connect: vi.fn().mockResolvedValue(mockClient)
+        },
+        __mockClient: mockClient
+    }   
+});
+
+const { categorizeTransaction } = await import("../openaiService.js");
+const {default: app} = await import("../app.js");
+const { __mockClient: mockClient } = await import("../db.js");
+
+//Good requests
+describe("POST /api/categorizer -good", () => {
+    beforeEach(()=>{
+        vi.clearAllMocks();
+    })
+
+    it("returns good status and also message, transactions and category name", async () =>{
+        mockClient.query
+            .mockResolvedValueOnce(undefined)
+            .mockResolvedValueOnce({rows:[{id: 1}]})
+            .mockResolvedValueOnce({rows:[{date:"2026-06-21", description:"Spotify Charge", amount: 19.99, category_id: 1, rawRowJson: { source: "manual_ai_input", api_used: "gpt-4o-mini" }}]})
+            .mockResolvedValueOnce(undefined)
+
+            const res = await request(app).post("/api/categorizer").send({amount: "19.99", description:"Spotify Charge", date:"2026-06-21"});
+
+            expect(res.status).toBe(200);
+            expect(res.body).toStrictEqual({
+                message: "Transaction saved",
+                transactions: [{date:"2026-06-21", description:"Spotify Charge", amount: 19.99, category_id: 1, rawRowJson: { source: "manual_ai_input", api_used: "gpt-4o-mini" }}],
+                category_name: "Subscriptions",
+            })
+            expect(categorizeTransaction).toHaveBeenCalledWith("Spotify Charge", "19.99");
+            expect(mockClient.release).toHaveBeenCalled();
+    });
+});
+
+//Bad requests
+describe("POST /api/categorizer -bad",  () => {
     it("returns 400 when description is missing", async () =>{
         const res = await request(app).post("/api/categorizer").send({amount: "0.00", date:"2026-01-02", description:""})
 
@@ -26,3 +80,6 @@ describe("POST api categorizer", async () => {
     });
 
 });
+
+
+
