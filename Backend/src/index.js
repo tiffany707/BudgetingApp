f70efdfa5fced9692f31da86e792ffa627ff84e1
tiffany707 <tiffanyokura7@gmail.com>
2026-07-09 19:39:30 -0600

    created kubernetes deployment for the backend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,4 +1,4 @@
 // backend/src/index.js
 import app from "./app.js";
 
-app.listen(5000, () => console.log("listening on port 5000"));
+app.listen(5000, "0.0.0.0",  () => console.log("listening on port 5000"));
